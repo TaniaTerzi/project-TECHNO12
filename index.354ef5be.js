b01397b4f7b00799ca8b0e0914f20c65bd7eeab7
TaniaTerzi <118296956+TaniaTerzi@users.noreply.github.com>
2023-05-05 17:43:41 +0000

    Deploying to gh-pages from @ TaniaTerzi/project-TECHNO12@55235f97077151597a708947b56cdba4ef0fd57f 🚀

--- index.354ef5be.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".slider");e.addEventListener("click",(function(){e.classList.toggle("active")})),document.querySelector(".slider").addEventListener("click",(function(){document.body.classList.toggle("dark-mode"),document.querySelector(".container-header").classList.toggle("dark-mode")}))}();
-//# sourceMappingURL=index.354ef5be.js.map
--- index.62a9dff1.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".slider");e.addEventListener("click",(()=>{e.classList.toggle("active")}));document.querySelector(".slider").addEventListener("click",(()=>{document.body.classList.toggle("dark-mode"),document.querySelector(".container-header").classList.toggle("dark-mode")}));
-//# sourceMappingURL=index.62a9dff1.js.map
