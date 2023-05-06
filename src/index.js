c26f423f36596eafa366ac20e0e1979f528e681d
TaniaTerzi <terzitetyana@gmail.com>
2023-05-06 21:32:57 +0200

    Summary
    Підключила скрипт, щоб рендирилися книги на головній сторінці

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import { slider, toggleSwitch } from './js/toggle.js';
 import { scrollButton } from './js/scroll-up.js';
 import { renderSupportMarkup } from './js/suppurt-slider.js'
+import { renderingBooksCategories, renderingCategory } from './js/booksRendering.js';
