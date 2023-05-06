333167ec79fee4d9823b1e84b5bdbbbef5b5fd35
Kirill_Halahan <Galagan.k.a.01@gmail.com>
2023-05-06 12:28:32 +0300

    support-slider

--- src/js/support-list.js
+++ src/js/support-list.js
@@ -3,47 +3,47 @@ const supportList = [
     title: 'Save the Children',
     url:
       'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
-    img: null,
+    img: './images/support/children.png',
   },
   {
     title: 'Project HOPE',
     url: 'https://www.projecthope.org/country/ukraine/',
-    img: null,
+    img: './images/support/hope_x1.png',
   },
   {
     title: 'UNITED24',
     url: 'https://u24.gov.ua/uk',
-    img: null,
+    img: './images/support/united_x1.png',
   },
   {
     title: 'International Medical Corps',
     url: 'https://internationalmedicalcorps.org/country/ukraine/',
-    img: null,
+    img: './images/support/medical_x1.png',
   },
   {
     title: 'Medicins Sans Frontieres',
     url: 'https://www.msf.org/ukraine',
-    img: null,
+    img: './images/support/sans_x1.png',
   },
   {
     title: 'RAZOM',
     url: 'https://www.razomforukraine.org/',
-    img: null,
+    img: './images/support/razom_x1.png',
   },
   {
     title: 'Action against hunger',
     url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
-    img: null,
+    img: './images/support/action_x1.png',
   },
   {
     title: 'World vision',
     url: 'https://www.wvi.org/emergencies/ukraine',
-    img: null,
+    img: './images/support/world_x1.png',
   },
   {
     title: 'Serhiy Prytula Charity Foundation',
     url: 'https://prytulafoundation.org/en',
-    img: null,
+    img: './images/support/prytula_x1.png',
   },
 ]
 
--- src/js/suppurt-slider.js
+++ src/js/suppurt-slider.js
@@ -1,23 +1,27 @@
 import { supportList } from "./support-list"
-import Swiper from 'swiper'
 
-sliderRefs = {
-    sliderWrapper: document.querySelector('.swiper-wrapper'),
-}
+
+
+
+const sliderWrapper = document.querySelector('.swiper-wrapper');
+
 
 renderSupportMarkup();
 function renderSupportMarkup() {
     let supportListId = 0;
     supportMarcup = supportList.map(({ title, url, img }) => {
-        supportListId +=1
+        supportListId += 1
         return `
             <div class="swiper-slide">
-                <a class="support-link" href="${url}" target="_blank"><span class="support-id" >${String(supportListId).padStart(2, 0)}</span>${title}</a>
+                <a class="support-link" href="${url}" target="_blank"><span class="support-id" >${String(supportListId).padStart(2, 0)}</span><img class="support-img" src="${img}" alt="${title}"></a>
             </div>
         `
     }).join('');
     console.log(supportMarcup);
-    sliderRefs.sliderWrapper.insertAdjacentHTML('afterbegin', supportMarcup);
+    sliderWrapper.insertAdjacentHTML('afterbegin', supportMarcup);
 };
 
-export { sliderRefs, renderSupportMarkup }
+
+
+
+export { renderSupportMarkup }
