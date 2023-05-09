6f252faebfcdea633a61a50e2a5ef9b9155a2e6b
TaniaTerzi <terzitetyana@gmail.com>
2023-05-09 13:49:51 +0200

    summary

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import { renderSupportMarkup } from './js/suppurt-slider.js';
 import { renderingBooksCategories, renderingCategory } from './js/booksRendering.js';
 import { booksCard } from './js/modal_book_card.js';
 import  {createCategoryList } from './js/categories.js';
-// import  {onShoppingList } from './js/shoppinglist.js'
+import  {onShoppingList } from './js/shoppinglist.js'
 // import  { modalOpenRagister, modalCloseRagister } from './js/registration.js'
