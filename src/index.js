12285c970930d9de9186d67d25eded7843bf1c79
ViktoriaDufynets <52819403+ViktoriaDufynets@users.noreply.github.com>
2023-05-10 11:34:39 +0300

    improvements

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // import { showLoadingIndicator, removeLoader } from './js/loader';
 import { slider, toggleSwitch } from './js/toggle.js';
 import { scrollButton } from './js/scroll-up.js';
+import { userLogin, userLogout } from './js/userbar.js';
 import { renderSupportMarkup } from './js/suppurt-slider.js';
 import {
   renderingBooksCategories,
@@ -10,5 +11,5 @@ import { booksCard } from './js/modal_book_card.js';
 // import { showLoadingIndicator } from './js/loader.js';
 import { createCategoryList } from './js/categories.js';
 import { onShoppingList } from './js/shoppinglist.js';
-import { userLogin, userLogout } from './js/userbar.js';
+
 // import  { modalOpenRagister, modalCloseRagister } from './js/registration.js'
