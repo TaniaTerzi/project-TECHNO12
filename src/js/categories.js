a890e320b6392e02546278d30f3a3bff20d2c5d2
8wondrs <8wondrs@gmail.com>
2023-05-10 21:22:40 +0300

    add filter with bugs

--- src/js/categories.js
+++ src/js/categories.js
@@ -2,51 +2,87 @@ import { fetchBooks } from './categoriesFetchRequest';
 
 const allCategoriesBtn = document.getElementById('allCategoriesBtn');
 const [listEl] = document.getElementsByClassName('categories-list');
-const mainListEl = document.querySelector('.main__list-js');
-const mainTitle = document.querySelector('.main__title-js');
+const mainListEl = document.querySelector('.top-books');
+const mainTitle = document.querySelector('.title-category');
 
-let idBook = 0;
+let handleImgClick;
 let title = '';
+
 allCategoriesBtn.classList.add('active-category');
 listEl.addEventListener('click', markup);
 
-const createCategoryList = async () => {
+async function createCategoryList() {
   try {
     const categoriesList = await fetchBooks.getCategoriesList();
     const makeNewButtons = categoriesList
       .map(
         category =>
-          `<li class= 'categories-list__item '> <button class= 'categories-list__button'>${category.list_name}</button> </li>`
+          `<li class='categories-list__item'><button class='categories-list__button'>${category.list_name}</button></li>`
       )
       .join('');
     listEl.insertAdjacentHTML('beforeend', makeNewButtons);
   } catch (error) {
     console.log(error);
   }
-};
+}
 createCategoryList();
 
-const addEventListenerForBook = book => {
+function addEventListenerForBook(book) {
   book.forEach(el => {
     el.addEventListener('click', handleImgClick);
   });
-};
+}
 
-const drawCategory = async nameOfCategory => {
+function makeMarkupCategory(books) {
+  return books
+    .map(
+      book => `
+        <li class="item-category-book" data-aos="flip-left" data-aos-anchor-placement="top-bottom" data-aos-once="true" data-book-id="${book._id}">
+          <a class="link-books-render" href="#" onclick="event.preventDefault()">
+            <div class="card-book">
+              <div class="img-card-book">
+                <img src="${book.book_image}" alt="book" class="img-book" loading="lazy">
+                <p class="book-card overlay">Quick view</p>
+              </div>
+              <div class="box-text-book-best">
+                <div class="box-title">
+                  <p class="title-book">${book.title}</p>
+                </div>
+                <div class="box-author">
+                  <p class="author-book">${book.author}</p>
+                </div>
+              </div>
+            </div>
+          </a>
+        </li>
+      `
+    )
+    .join('');
+}
+
+async function drawCategory(nameOfCategory) {
   const booksCategories = await fetchBooks.getBooksByCategory(nameOfCategory);
   const titleArr = nameOfCategory.split(' ');
   const titleFirstPart = titleArr.slice(0, titleArr.length - 1).join(' ');
   const titleLastPart = titleArr.slice(titleArr.length - 1).join();
 
-  // !!!!!!!!!!!!!!!!!!!!Сюди вставити функцію створення категорії!!!!!!!!!!!!!!!!!!!!
+  const markup = makeMarkupCategory(booksCategories);
+  mainListEl.innerHTML = markup;
+  mainListEl.previousElementSibling.remove();
+  mainListEl.insertAdjacentHTML('beforeend', markup);
 
-  //   const markup = makeMarkupGategory(booksCategories);
-  //   mainListEl.innerHTML = markup;
+  const lastWordMarkup = `<span class="title-best-sellers-color">${titleLastPart}</span>`;
+  const titleMarkup = `${titleFirstPart} ${lastWordMarkup}`;
 
-  const bookCategoryEl = document.querySelectorAll('.category-books__item');
+  mainListEl.insertAdjacentHTML(
+    'beforebegin',
+    `<h2 class="title-category">${titleMarkup}</h2>`
+  );
+
+  const bookCategoryEl = document.querySelectorAll('.categories-list__item');
   addEventListenerForBook(bookCategoryEl);
-  allCategoriesBtn.classList.remove('active-categories');
-};
+  allCategoriesBtn.classList.remove('active-category');
+}
 
 function markup(ev) {
   if (ev.target.nodeName !== 'BUTTON') {
@@ -57,9 +93,36 @@ function markup(ev) {
 
   if (ev.target === allCategoriesBtn) {
     allCategoriesBtn.classList.add('active-category');
+    const showAllCategories = async () => {
+      try {
+        const categoriesList = await fetchBooks.getCategoriesList();
+        const categoriesMarkup = categoriesList
+          .map(
+            category => `
+              <div class='categories-block'>
+                <h2 class='title-category'>${category.list_name}</h2>
+                <ul class='categories-list'>
+                  ${makeMarkupCategory(category.books)}
+                </ul>
+              </div>
+            `
+          )
+          .join('');
 
-    // !!!!!!!!!!!!!!!!!!!!Сюди вставити функцію створення усіх категорій!!!!!!!!!!!!!!!!!!!!
-    // showAllCategories();
+        if (mainTitle !== null) {
+          mainTitle.textContent = nameOfCategory;
+          mainListEl.innerHTML = categoriesMarkup;
+        }
+
+        const bookCategoryEl = document.querySelectorAll(
+          '.categories-list__item'
+        );
+        addEventListenerForBook(bookCategoryEl);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    showAllCategories();
 
     return;
   }
@@ -71,7 +134,6 @@ function markup(ev) {
 const clearSelectedCategories = () => {
   for (let i = 0; i < listEl.children.length; i += 1) {
     const category = listEl.children[i];
-
     category.firstElementChild.classList.remove('active-category');
   }
 };
